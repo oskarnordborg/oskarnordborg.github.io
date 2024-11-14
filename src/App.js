a7105624ac154ff5cfa4a115f7f9000b9c20ce40
Oskar Nordborg <47498943+oskarnordborg@users.noreply.github.com>
2024-11-14 22:44:24 +0100

    countdown

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Countdown from "./components/Countdown";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Årsrapporten</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Countdown />
+        <h1>Årsrapporten 2025</h1>
       </header>
     </div>
   );
