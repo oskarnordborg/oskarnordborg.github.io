21c53e8ffdbf221ca8bc5997c18865e20ff2a43d
Oskar Nordborg <47498943+oskarnordborg@users.noreply.github.com>
2025-01-14 22:07:34 +0100

    fix: styling signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -7,8 +7,10 @@ const SignUp = ({ pageData }) => {
   }
   return (
     <div className="section">
-      <h1>{pageData.signup_header || "signup.signup_header"}</h1>
-      <p>{pageData.signup_text}</p>
+      <h1 className="signup-h1">
+        {pageData.signup_header || "signup.signup_header"}
+      </h1>
+      {pageData.signup_text && <p>{pageData.signup_text}</p>}
       <iframe
         title="Anmälan 2025"
         src="https://docs.google.com/forms/d/e/1FAIpQLSdmjJxhSOAVM8diVJxjORkNEYg0JX0Jk-iA8U56SAWxXA141w/viewform"
